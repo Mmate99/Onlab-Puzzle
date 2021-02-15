70e4d391e478f330e5fbabe527aaa33007634d9d
Máté Martonosi <martonosi.mate@autsoft.hu>
2021-02-15 17:20:25 +0100

    Elso commmit

---
+++ Rush Hour/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rush_Hour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
